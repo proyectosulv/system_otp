4870f8d67c16fd6e1d89dde39dd4bb44a53fc2db
Waldir Ozuna <wozuna@hotmail.com>
2024-09-16 14:31:10 -0600

    correo quitando opt app2

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router();
 
 const userRoutes = require("./../domain/user");
 const OTPRoutes = require("./../domain/otp");
-const OTPAppRoutes = require("./../domain/otp_app");
+//const OTPAppRoutes = require("./../domain/otp_app");
 const EmailVerificationRoutes = require("./../domain/email_verification");
 const ForgotPasswordRoutes = require("./../domain/forgot_password");
 const ForgotPasswordAppsRoutes = require("./../domain/forgot_password_app");
 
 router.use("/user", userRoutes);
 router.use("/otp", OTPRoutes);
-router.use("/otp_app",OTPAppRoutes);
+//router.use("/otp_app",OTPAppRoutes);
 router.use("/email_verification", EmailVerificationRoutes);
 router.use("/forgot_password", ForgotPasswordRoutes);
 router.use("/forgot_password_app",  ForgotPasswordAppsRoutes);
